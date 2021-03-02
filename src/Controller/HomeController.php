c43a9509deef4ee04365b7f124bfe6d6fd3ff5ed
jawaher92 <jawaher.bouzazi@esprit.tn>
2021-03-02 08:52:14 +0100

    first push

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends  Controller
+{
+
+    /**
+     * @Route ("/")
+     * @Method ({"GET", "POST"})
+     */
+    public function index() {
+        return $this->render('home/index.html.twig');
+    }
+
+}
